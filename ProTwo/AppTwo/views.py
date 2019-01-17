aab68b5f745eb7e40b144ab4128cd56706d4daa6
Robert Lowe <robertmlowe@rmlowe.com>
2019-01-18 01:11:15 +0800

    Django Level Two Project Solutions

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from AppTwo.models import User
-
 # Create your views here.
 
 def index(request):
     return render(request,'AppTwo/index.html')
 
-def help(request):
-    helpdict = {'help_insert':'HELP PAGE'}
-    return render(request,'AppTwo/help.html',context=helpdict)
-
 def users(request):
-    return render(request,'AppTwo/users.html',context={'users':User.objects.order_by('first_name')})
+
+    user_list = User.objects.order_by('first_name')
+    user_dict = {'users':user_list}
+    return render(request,'AppTwo/users.html',context=user_dict)
--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
 from AppTwo import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^admin/', admin.site.urls),
     url(r'^users/',include('AppTwo.urls')),
-    path('admin/', admin.site.urls),
 ]
