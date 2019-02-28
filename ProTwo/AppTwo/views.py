d16da8f2000e60091369b058ec398bae3de7c697
Robert Lowe <robertmlowe@rmlowe.com>
2019-03-01 03:46:21 +0800

    Model Forms Exercise

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from AppTwo.models import User
+# from django.http import HttpResponse
+# from AppTwo.models import User
+from AppTwo.forms import NewUserForm
 # Create your views here.
 
 def index(request):
@@ -7,6 +9,15 @@ def index(request):
 
 def users(request):
 
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users':user_list}
-    return render(request,'AppTwo/users.html',context=user_dict)
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+
+    return render(request,'AppTwo/users.html',{'form':form})
